8f03e2e517ba62c4e92221c46826cfa2c8c89f0d
Akshat Bhuryan <me@here.com>
2022-09-05 00:24:58 +0530

    variables added

---
+++ web-22-variables-1/problem2.js
@@ -0,0 +1,7 @@
+var name = "Akshat Bhuryan";
+console.log(name);
+name= "Pramod Kumar Saini";
+console.log(name);
+name = "Manju Saini";
+console.log(name);
+
---
+++ web-22-variables-1/problem3.js
@@ -0,0 +1,5 @@
+var name = "Akshat Bhuryan";
+var age = 22;
+
+console.log("The name is "+name+" and the age is "+age);
+console.log("Type of the name is "+typeof(name)+" and type of age is  "+typeof(age));
